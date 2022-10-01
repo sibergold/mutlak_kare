21dc1084b2bb8e22f0a48b1ff0a7849fc1a00aa3
sibergold <kaynakca21@hotmail.com>
2022-10-01 21:45:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace mutlak_kare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  double kucukler=0;
+           double buyukler=0;
+            Console.WriteLine("Kaç sayı gireceksin?");
+            int n=Convert.ToInt32(Console.ReadLine());
+            double[] sayilar=new double[n];
+            for(int i=0;i<n;i++)
+            {   Console.WriteLine("Lütfen {0}. sayıyı gir.",(i+1));
+                sayilar[i]=Convert.ToDouble(Console.ReadLine());
+
+            }
+        for(int j=0;j<n;j++)
+        {
+            if(sayilar[j]<67)
+            {
+                kucukler+=67-sayilar[j];
+            
+            }
+         else if(sayilar[j]>67)
+         {
+            buyukler+=Math.Pow((sayilar[j]-67),2);
+         }
+         else
+         {
+            Console.WriteLine("0");
+         }
+        
+        }
+        Console.WriteLine("Küçüklerin Farklarının Toplamı:{0} ve Büyüklerin Farklarının Karelerinin Toplamı:{1}",kucukler,buyukler);
+        
+        }
+    }
+}
